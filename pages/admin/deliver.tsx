4099447fef512eb5d221f76879a7bcaa076e60b5
Camiloignaval <camiloignaval@gmail.com>
2022-09-24 00:49:40 -0300

    se agrega paginacion a ordenes en camino

--- pages/admin/deliver.tsx
+++ pages/admin/deliver.tsx
@@ -17,8 +17,12 @@ import {
 } from "../../store/RTKQuery/ordersApi";
 
 const DeliverPage = () => {
-  const { data: dataOrders } = useGetAllOrdersQuery("status=dispatched");
   const [changeStatus, changeStatusState] = useChangeOrderStatusMutation();
+  const [page, setPage] = React.useState(0);
+  const [pageSize, setPageSize] = React.useState(20);
+  const { data: dataOrders, isLoading } = useGetAllOrdersQuery(
+    `status=dispatched&page=${page + 1}&limit=${pageSize}`
+  );
 
   console.log({ dataOrders });
   const columns: GridColDef[] = [
@@ -43,6 +47,7 @@ const DeliverPage = () => {
               changeStatus({ ids: [row.id], newStatus: "delivered" })
             }
             color="success"
+            disabled={changeStatusState.isLoading}
           >
             <DoneAll />
           </IconButton>
@@ -50,6 +55,16 @@ const DeliverPage = () => {
       },
     },
   ];
+  const [rowCountState, setRowCountState] = React.useState(
+    dataOrders?.totalDocs || 0
+  );
+  React.useEffect(() => {
+    setRowCountState((prevRowCountState) =>
+      dataOrders?.totalDocs !== undefined
+        ? dataOrders?.totalDocs
+        : prevRowCountState
+    );
+  }, [dataOrders?.totalDocs, setRowCountState]);
 
   if (!dataOrders) return <FullScreenLoading />;
 
@@ -79,9 +94,22 @@ const DeliverPage = () => {
         width="100%"
       >
         <DataGrid
+          rowCount={rowCountState}
+          loading={isLoading}
+          rowsPerPageOptions={[10, 20, 30, 50, 70, 100]}
+          pagination
+          page={page}
+          pageSize={pageSize}
+          paginationMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
           localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+          disableSelectionOnClick
           rows={rows ?? []}
           columns={columns}
+          componentsProps={{
+            pagination: { classes: null },
+          }}
         ></DataGrid>
       </Grid>
     </AdminLayout>

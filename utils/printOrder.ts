c6469feae977e8d065a23926f9ffa2e9c897f79f
Camiloignaval <camiloignaval@gmail.com>
2022-10-04 22:56:57 -0300

    probando impresion

--- utils/printOrder.ts
+++ utils/printOrder.ts
@@ -1,9 +1,9 @@
 import { IProduct } from "./../interfaces/products";
 import { IOrder } from "./../interfaces/order";
 import { ConectorPlugin } from "./conectorPlugin";
+import { ConectorPluginV3 } from "./conectorPluginv3";
 import { currency } from ".";
 import { toast } from "react-hot-toast";
-import { connector_plugin } from "./plugin_impresora_abraza";
 interface IProductCustomRoll {
   name?: string;
   price?: number;
@@ -11,73 +11,67 @@ interface IProductCustomRoll {
 }
 
 // export const printOrder = async () => {
-//   let nombreImpresora = "ImpresoraTermica";
-//   let api_key = "12345";
-
-//   const conector = new connector_plugin();
-//   conector.fontsize("2");
-//   conector.textaling("center");
-//   conector.text("Ferreteria Angel");
-//   conector.fontsize("1");
-//   conector.text("Calle de las margaritas #45854");
-//   conector.text("PEC7855452SCC");
-//   conector.feed("3");
-//   conector.textaling("left");
-//   conector.text("Fecha: Miercoles 8 de ABRIL 2022 13:50");
-//   conector.text("Cant.       Descripcion      Importe");
-//   conector.text("------------------------------------------");
-//   conector.text("1- Barrote 2x4x8                    $110");
-//   conector.text("3- Esquinero Vinil                  $165");
-//   conector.feed("1");
-//   conector.fontsize("2");
-//   conector.textaling("center");
-//   conector.text("Total: $275");
-//   conector.qr("https://abrazasoft.com");
-//   conector.feed("5");
-//   conector.cut("0");
-
-//   const resp = await conector.imprimir(nombreImpresora, api_key);
-//   if (resp === true) {
-//   } else {
-//     console.log("Problema al imprimir: " + resp);
-//   }
+//   const nombreImpresora = "ImpresoraTermica"; // Puede ser obtenida de la lista de impresoras o puedes escribirlo si lo conoces
+//   const conector = new ConectorPluginV3();
+//   const respuesta = await conector
+//     .Iniciar()
+//     .EscribirTexto("Hola mundo")
+//     .Feed(1)
+//     .imprimirEn(nombreImpresora);
 // };
+const URLPlugin = window.document.location.origin;
 
 export const printOrder = async (order: IOrder) => {
-  const conector = new ConectorPlugin();
-  conector
-    .establecerTamanioFuente(2, 2)
-    .establecerEnfatizado(1)
-    .establecerJustificacion(ConectorPlugin.Constantes.AlineacionCentro)
-    .textoConAcentos("Sushi Panko\n")
-    .feed(2);
+  const nombreImpresora = "ImpresoraTermica"; // Puede ser obtenida de la lista de impresoras o puedes escribirlo si lo conoces
+
+  const conector = new ConectorPluginV3();
+  const respuesta = await conector
+    .Iniciar()
+    .EstablecerTamañoFuente(2, 2)
+    .EstablecerEnfatizado(true)
+    .EstablecerAlineacion(ConectorPluginV3.ALINEACION_CENTRO)
+    .EscribirTexto("Sushi Panko\n")
+    .Feed(2);
 
   // datos cliente
   conector
-    .establecerEnfatizado(0)
-    .establecerTamanioFuente(1, 1)
-    .establecerJustificacion(ConectorPlugin.Constantes.AlineacionIzquierda)
-    .textoConAcentos(`Cliente: ${order.shippingAddress.username}\n`)
-    .textoConAcentos(`Direccion: ${order.shippingAddress.address}\n`)
-    .feed(1);
-  order.orderItems.forEach((item) => {
+    .EstablecerEnfatizado(false)
+    .EstablecerTamañoFuente(1, 1)
+    .EstablecerAlineacion(ConectorPluginV3.ALINEACION_IZQUIERDA)
+    .TextoSegunPaginaDeCodigos(
+      2,
+      "cp850",
+      `Cliente: ${order.shippingAddress.username}\n`
+    )
+    .TextoSegunPaginaDeCodigos(
+      2,
+      "cp850",
+      `Dirección: ${order.shippingAddress.address}\n`
+    )
+    .Feed(1);
+  order.orderItems.forEach((item, i) => {
+    console.log(i, item);
     conector
-      .establecerEnfatizado(0)
-      .establecerJustificacion(ConectorPlugin.Constantes.AlineacionIzquierda)
-      .textoConAcentos(`${item.quantity} ${item.name}\n`);
+      .EstablecerEnfatizado(false)
+      .EstablecerAlineacion(ConectorPluginV3.ALINEACION_IZQUIERDA)
+      .EscribirTexto(`${item.quantity} ${item.name}\n`);
     if (item.name === "Roll personalizado") {
       // imprimir ingredientes
       // envoltura
       conector
-        .establecerJustificacion(ConectorPlugin.Constantes.AlineacionIzquierda)
-        .establecerEnfatizado(1)
-        .textoConAcentos(" -Envoltura: ");
+        .EstablecerAlineacion(ConectorPluginV3.ALINEACION_IZQUIERDA)
+        .EstablecerEnfatizado(true)
+        .EscribirTexto(" -Envoltura: ");
       item.envelopes &&
         item.envelopes.map((i: IProductCustomRoll) => {
-          conector.establecerEnfatizado(0).textoConAcentos(`${i.name}\n`);
+          conector
+            .EstablecerEnfatizado(false)
+            .TextoSegunPaginaDeCodigos(2, "cp850", `${i.name}\n`);
         });
       // relleno
-      conector.establecerEnfatizado(1).textoConAcentos(" -Relleno: ");
+      conector
+        .EstablecerEnfatizado(true)
+        .TextoSegunPaginaDeCodigos(2, "cp850", " -Relleno: ");
       if (item.proteins && item.vegetables) {
         const stringRelleno = [
           ...(item.proteins ?? []),
@@ -85,74 +79,85 @@ export const printOrder = async (order: IOrder) => {
         ]
           .map((i) => i.name)
           .join("-");
-        conector.establecerEnfatizado(0).textoConAcentos(`${stringRelleno}\n`);
+        conector
+          .EstablecerEnfatizado(false)
+          .TextoSegunPaginaDeCodigos(2, "cp850", `${stringRelleno}\n`);
       }
       // extras
       if (item?.extraProduct) {
-        conector.establecerEnfatizado(1).textoConAcentos(" -Extras: ");
+        conector
+          .EstablecerEnfatizado(true)
+          .TextoSegunPaginaDeCodigos(2, "cp850", " -Extras: ");
         const stringExtras = item.extraProduct
           .map((i: IProductCustomRoll) => i.name)
           .join("-");
-        conector.establecerEnfatizado(0).textoConAcentos(`${stringExtras}\n`);
+        conector
+          .EstablecerEnfatizado(false)
+          .TextoSegunPaginaDeCodigos(2, "cp850", `${stringExtras}\n`);
       }
       // Salsas
       // TODO ARREGLAR
-      // conector.establecerEnfatizado(1).textoConAcentos(" -Salsas: ");
-      // if (item.sauces) {
-      //   const stringRelleno = item.sauces
-      //     .map((i: IProductCustomRoll) =>
-      //       i?.name ? i.name.replace("Salsa de", "") : ""
-      //     )
-      //     .join("-");
-      //   conector.establecerEnfatizado(0).textoConAcentos(`${stringRelleno}\n`);
-      // }
+      conector.EstablecerEnfatizado(true).EscribirTexto(" -Salsas: ");
+      if (item.sauces) {
+        const stringRelleno = item.sauces
+          .map((i: IProductCustomRoll) =>
+            i?.name ? i.name.replace("Salsa de", "") : ""
+          )
+          .join("-");
+        conector
+          .EstablecerEnfatizado(false)
+          .TextoSegunPaginaDeCodigos(2, "cp850", `${stringRelleno}\n`);
+      }
     }
-    if (item?.note) {
+    if (item.note) {
       // notas
       conector
-        .establecerEnfatizado(1)
-        .establecerJustificacion(ConectorPlugin.Constantes.AlineacionCentro)
-        .textoConAcentos(`*${item?.note}*\n`);
+        .EstablecerEnfatizado(1)
+        .EstablecerAlineacion(ConectorPluginV3.ALINEACION_CENTRO)
+        .TextoSegunPaginaDeCodigos(2, "cp850", `*${item?.note}*\n`);
     }
-    conector.feed(1);
+    conector.Feed(1);
   });
 
   // productos extras
   if (order?.orderExtraItems!.length > 0) {
     conector
-      .establecerEnfatizado(0)
-      .establecerJustificacion(ConectorPlugin.Constantes.AlineacionIzquierda)
-      .texto(`Productos extra: \n`);
+      .EstablecerEnfatizado(false)
+      .EstablecerAlineacion(ConectorPluginV3.ALINEACION_IZQUIERDA)
+      .EscribirTexto(`Productos extra: \n`);
     order?.orderExtraItems?.map((ei) => {
-      conector.texto(` ${ei.quantity} ${ei.name}\n`);
+      conector.TextoSegunPaginaDeCodigos(
+        2,
+        "cp850",
+        ` ${ei.quantity} ${ei.name}\n`
+      );
     });
   }
-  conector.feed(1);
+  conector.Feed(1);
 
   // total
   conector
-    .establecerJustificacion(ConectorPlugin.Constantes.AlineacionDerecha)
-    .establecerEnfatizado(1)
-    .textoConAcentos("----------------------------\n")
-    .textoConAcentos(`Total:${currency.format(Math.round(order.total))}`)
-    .feed(3)
-    .establecerJustificacion(ConectorPlugin.Constantes.AlineacionCentro)
-    .textoConAcentos("***Gracias por su compra***\n")
-    .feed(1)
-    .establecerEnfatizado(0)
-    .establecerJustificacion(ConectorPlugin.Constantes.AlineacionDerecha)
-    .texto(`N° ${order._id!.slice(-10)}`)
-    .feed(1)
-    .cortar()
-    .cortarParcialmente()
-    .imprimirEn("ImpresoraTermica")
-    .then((respuestaAlImprimir) => {
-      if (respuestaAlImprimir === true) {
-        console.log("Impreso correctamente");
-      } else {
-        console.log("Error. La respuesta es: " + respuestaAlImprimir);
-      }
-    });
+    .EstablecerAlineacion(ConectorPluginV3.ALINEACION_DERECHA)
+    .EstablecerEnfatizado(true)
+    .EscribirTexto("----------------------------\n")
+    .EscribirTexto(`Total:${currency.format(Math.round(order.total))}`)
+    .Feed(3)
+    .EstablecerAlineacion(ConectorPluginV3.ALINEACION_CENTRO)
+    .EscribirTexto("***Gracias por su compra***\n")
+    .Feed(1)
+    .EstablecerEnfatizado(false)
+    .EstablecerAlineacion(ConectorPluginV3.ALINEACION_DERECHA)
+    .TextoSegunPaginaDeCodigos(2, "cp850", `N° ${order._id!.slice(-10)}`)
+    .Feed(1);
+  // .Corte()
+  // .CorteParcial()
+  // .Pulso(48, 60, 120)
+  // .imprimirEn(nombreImpresora);
+  if (respuesta) {
+    console.log("Impreso correctamente", JSON.stringify(respuesta));
+  } else {
+    console.log("Error: " + JSON.stringify(respuesta));
+  }
 };
 
 export const printIsConnected = async () => {
